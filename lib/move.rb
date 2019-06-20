e276c0897f94a542d591e168d0358edabcf88e36
Chetana Singhal <singhal.ct.1998@gmail.com>
2019-06-20 10:44:32 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(string input)
+def input_to_index(input)
   input.to_i
   number_adder(input)
   return input
